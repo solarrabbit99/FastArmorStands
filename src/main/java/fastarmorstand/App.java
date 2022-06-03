4ffbeb0cc6a7f0da093e6a4501735652e79e40be
zhenghanlee <hanhan_1999@hotmail.com>
2022-06-04 00:51:14 +0800

    Initial commit

---
+++ src/main/java/fastarmorstand/App.java
@@ -0,0 +1,13 @@
+package fastarmorstand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
